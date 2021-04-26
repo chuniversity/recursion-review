6b0218680f10f267e6fffd2b37b5529b8ad27f64
Chuniversity <contact@chunis.org>
2021-04-26 16:58:14 -0400

    initial commit

--- test.js
+++ test.js
@@ -4,13 +4,19 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function (obj) {
-  var result = '';
+  // your code goes here
+  var result = [];
   if (Array.isArray(obj)) {
     var arrayResults = [];
     obj.forEach(function (value) {
-      arrayResults.push(stringifyJSON(value));
+      result.push(stringifyJSON(value));
     });
-    return result += '[' + arrayResults.join(',') + ']'
   }
   return result;
 }
+
+
+
+
+var output = stringifyJSON([1, 2, 3])
+console.log(output)
