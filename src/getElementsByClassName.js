a92bedb60ddf1559a1701f2b42dc5b913278612a
Chuniversity <contact@chunis.org>
2021-04-27 00:13:02 -0400

    get elements by className

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,7 +6,16 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function (className
 ) {
-
-
-
+  var result = [];
+  var recursionFunction = function (element) {
+    var children = element.childNodes
+    if (element.classList && element.classList.contains(className)) {
+      result.push(element)
+    }
+    for (var i = 0; i < children.length; i++) {
+      recursionFunction(children[i])
+    }
+  }
+  recursionFunction(document)
+  return result
 };
